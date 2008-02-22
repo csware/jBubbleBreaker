6c9abe1225b54047e1a139f930b1802337ed90f5
Sven Strickroth <email@cs-ware.de>
2008-02-22 22:25:44 +0000

    correct header

--- source/org/jbubblebreaker/gamemodes/GameDefault.java
+++ source/org/jbubblebreaker/gamemodes/GameDefault.java
@@ -1,7 +1,19 @@
 /*
- * Sven Strickroth
- * Matrikelnummer: 352752
- * eMail: sven.strickroth@tu-clausthal.de
+ * Copyright 2008 Sven Strickroth <email@cs-ware.de>
+ * 
+ * This file is part of JBubbleBreaker.                                  
+ * 
+ * JBubbleBreaker is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3 as     
+ * published by the Free Software Foundation.                            
+ * 
+ * JBubbleBreaker is distributed in the hope that it will be useful,     
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of        
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         
+ * GNU General Public License for more details.                          
+ * 
+ * You should have received a copy of the GNU General Public License     
+ * along with JBubbleBreaker. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.jbubblebreaker.gamemodes;
 
